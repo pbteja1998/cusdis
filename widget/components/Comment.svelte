ef42d58fe703cbaf8f3088d3a8a5632ab07f46a2
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2022-08-05 17:35:35 +0530

    chore: viewbox fix

--- widget/components/Comment.svelte
+++ widget/components/Comment.svelte
@@ -26,7 +26,7 @@
             <svg
               class="h-full w-full text-gray-300"
               fill="currentColor"
-              view-box="0 0 24 24"
+              viewBox="0 0 24 24"
             >
               <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
             </svg>
@@ -59,7 +59,7 @@
         >
           <svg
             class="mr-2 h-4 w-4"
-            view-box="0 0 24 24"
+            viewBox="0 0 24 24"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
           >
