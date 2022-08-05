baa541f911382fcb1ba507e763f36dab7aff0593
Bhanu Teja Pachipulusu <pbteja1998@gmail.com>
2022-08-05 17:52:46 +0530

    chore: fix styling bug

--- widget/components/Comment.svelte
+++ widget/components/Comment.svelte
@@ -29,11 +29,11 @@
               viewBox="0 0 24 24"
             >
               <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
-            <p class="text-base font-bold text-gray-900">
-              {comment.moderator && comment.moderator.displayName ? comment.moderator.displayName : comment.by_nickname}
-            </p>
+            </svg>            
           </span>
+          <p class="text-base font-bold text-gray-900">
+            {comment.moderator && comment.moderator.displayName ? comment.moderator.displayName : comment.by_nickname}
+          </p>
         </div>
         <p class="text-xs font-semibold uppercase tracking-widest text-gray-500 mt-0">
           {formatDate(comment.parsedCreatedAt)}
